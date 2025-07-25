0da91b8a2fb8b6dd1204f7196fcc68e5967e8f98
Maggie <lemaggie2099@gmail.com>
2025-07-25 09:58:26 -0700

    Initial Commit

---
+++ BirthdayApp/BirthdayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayAppApp.swift
+//  BirthdayApp
+//
+//  Created by Maggie Le on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
